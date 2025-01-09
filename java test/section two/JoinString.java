30f39115d6a040363d9bbcef40677c5766e50c8e
prefonixs <sidharthmishra10503@gmail.com>
2025-01-09 20:24:10 +0530

    git: completed, java: upto switch case

---
+++ java test/section two/JoinString.java
@@ -0,0 +1,8 @@
+public class JoinString {
+    public static void main(String[] args) {
+        int year=2010;
+        String winner="Spain";
+        String announce="The winner of the "+year+" cup is "+winner;
+        System.out.println(announce);
+    }
+}
---
+++ java test/section two/WholeNumbers.java
@@ -0,0 +1,6 @@
+public class WholeNumbers {
+    public static void main(String[] args) {
+        long globalPopulation = 8_000_000_000L;
+        System.out.println("The global population is "+globalPopulation);
+    }
+}
